3081a009a6b95f4d3d185b5463b718465cf784fd
RishabhPurwar24 <rishabh15772@gmail.com>
2022-06-27 11:38:44 +0530

    Feat: Recipe Component added

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,27 @@
 import "./Recipe.css";
+import { useFetch } from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { error, isPending, data: recipe } = useFetch(url);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to make.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
